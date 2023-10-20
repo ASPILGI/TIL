f83dec8ac6475dbe6c5a980f82d8b2241715263a
이용관 (Lee YongGwan) <84302546+ASPILGI@users.noreply.github.com>
2023-10-20 09:35:27 +0900

    Update 12.sql

--- SQL/SELECT/12.sql
+++ SQL/SELECT/12.sql
@@ -1,1 +1,8 @@
+-- 배운점
+-- ifnull함수로 null일 때 값을 쉽게 값을 변경할 수 있다.
+-- ifnull(컬럼, null일때 바꾸고 싶은 값)
 
+SELECT PT_NAME, PT_NO, GEND_CD, AGE, IFNULL(TLNO, 'NONE') AS TLNO
+from PATIENT
+where GEND_CD = 'W' AND AGE <= 12
+ORDER BY AGE DESC, PT_NAME
